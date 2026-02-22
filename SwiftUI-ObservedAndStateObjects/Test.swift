ff6795341929ac8b15250bd7926eb1952d94520a
Krish Mishra <rm25101980@gmail.com>
2026-02-23 00:34:30 +0530

    Added test swift view

---
+++ SwiftUI-ObservedAndStateObjects/Test.swift
@@ -0,0 +1,18 @@
+//
+//  Test.swift
+//  SwiftUI-ObservedAndStateObjects
+//
+//  Created by Krish Mishra on 23/02/26.
+//
+
+import SwiftUI
+
+struct Test: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    Test()
+}
